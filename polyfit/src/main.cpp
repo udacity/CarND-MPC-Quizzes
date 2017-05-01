fb864692ab23856bf369b43f2a85f8818decb177
Dominique Luna <dluna132@gmail.com>
2017-04-30 20:26:18 -0700

    added polynomial fitting quiz

---
+++ polyfit/src/main.cpp
@@ -0,0 +1,52 @@
+// In this quiz you'll fit a polynomial to waypoints.
+
+#include <iostream>
+#include "Eigen-3.3/Eigen/Core"
+#include "Eigen-3.3/Eigen/QR"
+
+using namespace Eigen;
+
+double polyeval(Eigen::VectorXd coeffs, double x) {
+  double result = 0.0;
+  for (int i = 0; i < coeffs.size(); i++) {
+    result += coeffs[i] * pow(x, i);
+  }
+  return result;
+}
+
+Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
+                        int order) {
+  assert(xvals.size() == yvals.size());
+  assert(order >= 1 && order <= xvals.size() - 1);
+  Eigen::MatrixXd A(xvals.size(), order + 1);
+
+  for (int i = 0; i < xvals.size(); i++) {
+    A(i, 0) = 1.0;
+  }
+
+  for (int j = 0; j < xvals.size(); j++) {
+    for (int i = 0; i < order; i++) {
+      A(j, i + 1) = A(j, i) * xvals(j);
+    }
+  }
+
+  auto Q = A.householderQr();
+  auto result = Q.solve(yvals);
+  return result;
+}
+
+int main() {
+  Eigen::VectorXd xvals(6);
+  Eigen::VectorXd yvals(6);
+  // x waypoint coordinates
+  xvals << 9.261977, -2.06803, -19.6663, -36.868, -51.6263, -66.3482;
+  // y waypoint coordinates
+  yvals << 5.17, -2.25, -15.306, -29.46, -42.85, -57.6116;
+
+  // TODO: use `polyfit` to fit a third order polynomial to the (x, y)
+  // coordinates.
+
+  for (double x = 0; x <= 20; x += 1.0) {
+    // TODO: use `polyeval` to evaluate the x values.
+  }
+}
