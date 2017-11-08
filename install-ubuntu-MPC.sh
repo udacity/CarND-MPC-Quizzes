fe16779e8d1ee4dd32786149e3472cb7ac2591c2
Andrew <baumanab@gmail.com>
2017-11-08 11:28:33 -0800

    update Windows install instructions, add depency install script

---
+++ install-ubuntu-MPC.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# update
+sudo apt-get update
+
+# gfortran dependency
+sudo apt-get install gfortran
+
+# get unzip
+sudo apt-get install unzip
+
+# Ipopt: get, install, unzip
+wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.7.zip && unzip Ipopt-3.12.7.zip && rm Ipopt-3.12.7.zip
+./install_ipopt.sh
+
+# CppAD
+sudo apt-get install cppad
+
+# Gnuplot
+sudo apt-get install gnuplot
+
+# python and matplotlib
+sudo apt-get install python-matplotlib
+sudo apt-get install python2.7-dev
