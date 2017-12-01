d5396c2a4e3bbea8efc42670f67f3be95a63f4a2
Chien-Pin Chen <chienpinchen@gmail.com>
2017-12-01 03:43:42 -0800

    Update install-ubuntu-MPC.sh - add argument
    From [instruction](./install_Ipopt_CppAD.md), install_ipopt.sh need the Ipopt source directory as the first argument

--- install-ubuntu-MPC.sh
+++ install-ubuntu-MPC.sh
@@ -11,7 +11,7 @@ sudo apt-get install unzip
 
 # Ipopt: get, install, unzip
 wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.7.zip && unzip Ipopt-3.12.7.zip && rm Ipopt-3.12.7.zip
-./install_ipopt.sh
+./install_ipopt.sh Ipopt-3.12.7/
 
 # CppAD
 sudo apt-get install cppad
