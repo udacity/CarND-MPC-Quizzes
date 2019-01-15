f9ed1e13fb984145cf2388703c71a3868f23348d
mvirgo <mvirgs44@gmail.com>
2019-01-15 10:37:30 -0800

    Refactor: Enhanced coding style for polyfit
    Refactored the coding style of the polyfit quiz to have proper function order, clearer comments for the classroom, and use pre-incrementing

--- polyfit/solution/main.cpp
+++ polyfit/solution/main.cpp
@@ -4,59 +4,30 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 
-using namespace Eigen;
+using Eigen::VectorXd;
 
 // Evaluate a polynomial.
-double polyeval(Eigen::VectorXd coeffs, double x) {
-  double result = 0.0;
-  for (int i = 0; i < coeffs.size(); i++) {
-    result += coeffs[i] * pow(x, i);
-  }
-  return result;
-}
-
+double polyeval(VectorXd coeffs, double x);
 // Fit a polynomial.
-// Adapted from
-// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
-Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
-                        int order) {
-  assert(xvals.size() == yvals.size());
-  assert(order >= 1 && order <= xvals.size() - 1);
-  Eigen::MatrixXd A(xvals.size(), order + 1);
-
-  for (int i = 0; i < xvals.size(); i++) {
-    A(i, 0) = 1.0;
-  }
-
-  for (int j = 0; j < xvals.size(); j++) {
-    for (int i = 0; i < order; i++) {
-      A(j, i + 1) = A(j, i) * xvals(j);
-    }
-  }
-
-  auto Q = A.householderQr();
-  auto result = Q.solve(yvals);
-  return result;
-}
+VectorXd polyfit(VectorXd xvals, VectorXd yvals, int order);
 
 int main() {
-  Eigen::VectorXd xvals(6);
-  Eigen::VectorXd yvals(6);
+  VectorXd xvals(6);
+  VectorXd yvals(6);
   // x waypoint coordinates
   xvals << 9.261977, -2.06803, -19.6663, -36.868, -51.6263, -66.3482;
   // y waypoint coordinates
   yvals << 5.17, -2.25, -15.306, -29.46, -42.85, -57.6116;
 
   // Pass the x and y waypoint coordinates along the order of the polynomial.
-  // In this case, 3.
+  //   In this case, 3.
   auto coeffs = polyfit(xvals, yvals, 3);
 
-  for (double x = 0; x <= 20; x += 1.0) {
+  for (double x = 0; x <= 20; ++x) {
     // We can evaluate the polynomial at a x coordinate by calling `polyeval`.
-    // The first argument being the coefficients, `coeffs`.
-    // The second being the x coordinate, `x`.
-    auto v = polyeval(coeffs, x);
-    std::cout << v << std::endl;
+    //   The first argument being the coefficients, `coeffs`.
+    //   The second being the x coordinate, `x`.
+    std::cout << polyeval(coeffs, x) << std::endl;
   }
 
   // Expected output
@@ -81,4 +52,36 @@ int main() {
   // 10.6041
   // 11.2005
   // 11.7925
+}
+
+double polyeval(VectorXd coeffs, double x) {
+  double result = 0.0;
+  for (int i = 0; i < coeffs.size(); ++i) {
+    result += coeffs[i] * pow(x, i);
+  }
+  return result;
+}
+
+// Adapted from:
+// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
+VectorXd polyfit(VectorXd xvals, VectorXd yvals, int order) {
+  assert(xvals.size() == yvals.size());
+  assert(order >= 1 && order <= xvals.size() - 1);
+
+  Eigen::MatrixXd A(xvals.size(), order + 1);
+
+  for (int i = 0; i < xvals.size(); ++i) {
+    A(i, 0) = 1.0;
+  }
+
+  for (int j = 0; j < xvals.size(); ++j) {
+    for (int i = 0; i < order; ++i) {
+      A(j, i + 1) = A(j, i) * xvals(j);
+    }
+  }
+
+  auto Q = A.householderQr();
+  auto result = Q.solve(yvals);
+
+  return result;
 }
--- polyfit/src/main.cpp
+++ polyfit/src/main.cpp
@@ -4,54 +4,34 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 
-using namespace Eigen;
+using Eigen::VectorXd;
 
 // Evaluate a polynomial.
-double polyeval(Eigen::VectorXd coeffs, double x) {
-  double result = 0.0;
-  for (int i = 0; i < coeffs.size(); i++) {
-    result += coeffs[i] * pow(x, i);
-  }
-  return result;
-}
-
+double polyeval(VectorXd coeffs, double x);
 // Fit a polynomial.
-// Adapted from
-// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
-Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
-                        int order) {
-  assert(xvals.size() == yvals.size());
-  assert(order >= 1 && order <= xvals.size() - 1);
-  Eigen::MatrixXd A(xvals.size(), order + 1);
-
-  for (int i = 0; i < xvals.size(); i++) {
-    A(i, 0) = 1.0;
-  }
-
-  for (int j = 0; j < xvals.size(); j++) {
-    for (int i = 0; i < order; i++) {
-      A(j, i + 1) = A(j, i) * xvals(j);
-    }
-  }
-
-  auto Q = A.householderQr();
-  auto result = Q.solve(yvals);
-  return result;
-}
+VectorXd polyfit(VectorXd xvals, VectorXd yvals, int order);
 
 int main() {
-  Eigen::VectorXd xvals(6);
-  Eigen::VectorXd yvals(6);
+  VectorXd xvals(6);
+  VectorXd yvals(6);
   // x waypoint coordinates
   xvals << 9.261977, -2.06803, -19.6663, -36.868, -51.6263, -66.3482;
   // y waypoint coordinates
   yvals << 5.17, -2.25, -15.306, -29.46, -42.85, -57.6116;
 
-  // TODO: use `polyfit` to fit a third order polynomial to the (x, y)
-  // coordinates.
+  /**
+   * TODO: use `polyfit` to fit a third order polynomial to the (x, y)
+   *   coordinates.
+   * Hint: call Eigen::VectorXd polyfit() and pass xvals, yvals, and the 
+   *   polynomial degree/order
+   */
+  // YOUR CODE HERE
 
-  for (double x = 0; x <= 20; x += 1.0) {
-    // TODO: use `polyeval` to evaluate the x values.
+  for (double x = 0; x <= 20; ++x) {
+    /**
+     * TODO: use `polyeval` to evaluate the x values.
+     */
+    std::cout << "YOUR CODE HERE" << std::endl; 
   }
 
   // Expected output
@@ -76,4 +56,36 @@ int main() {
   // 10.6041
   // 11.2005
   // 11.7925
+}
+
+double polyeval(VectorXd coeffs, double x) {
+  double result = 0.0;
+  for (int i = 0; i < coeffs.size(); ++i) {
+    result += coeffs[i] * pow(x, i);
+  }
+  return result;
+}
+
+// Adapted from:
+// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
+VectorXd polyfit(VectorXd xvals, VectorXd yvals, int order) {
+  assert(xvals.size() == yvals.size());
+  assert(order >= 1 && order <= xvals.size() - 1);
+
+  Eigen::MatrixXd A(xvals.size(), order + 1);
+
+  for (int i = 0; i < xvals.size(); ++i) {
+    A(i, 0) = 1.0;
+  }
+
+  for (int j = 0; j < xvals.size(); ++j) {
+    for (int i = 0; i < order; ++i) {
+      A(j, i + 1) = A(j, i) * xvals(j);
+    }
+  }
+
+  auto Q = A.householderQr();
+  auto result = Q.solve(yvals);
+
+  return result;
 }
